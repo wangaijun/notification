9d59efef73a900825e0a54f51b2b7439ea5d912f
wangaijun <hw_wangaijun@163.com>
2018-12-28 19:47:10 +0800

    显示通知列表

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <x-header :left-options="{showBack:false,}">
+    <x-header :left-options="{showBack:false,backText:'',}">
       <span>通知列表</span>
       <i slot="right" @click="on_plus_btn_click" class="iconfont  icon-jiahao"></i>
     </x-header>
+    <div>
+      
+      {{list[0]}}
+    </div>
   </div>
 </template>
 
@@ -16,6 +20,7 @@ export default {
   },
   data () {
     return {
+      list:[],
     }
   },
   methods:{
@@ -23,6 +28,10 @@ export default {
       console.log("on click");
       this.$router.push('/AddNotification')
     },
+  },
+  mounted:function(){
+    let s = '[{"noticeId":1583100951398439,"content":"通知内容","noticeType":1,"status":0,"createDate":1545997022000,"resourceId":null,"updateDate":null,"creatorId":"6458251148183884119","responsibleId":"6458251148183884119","subcontractorId":"1581062192135033","participants":"6298842148508439286,6225576885289685719","remark":"备注","noticeDate":1545996992000,"isToAllMember":0,"isAdd":1,"creatorName":"罗少帅","responsibleName":"罗少帅","finishRead":0}]'
+    this.list = JSON.parse(s)
   }
 }
 </script>
