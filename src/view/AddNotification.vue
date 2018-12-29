e0c6f139ce4ee040939aaa0e8f7e120cfb041533
wangaijun <wangaijun@163.com>
2018-12-29 09:47:48 +0800

    1

---
+++ src/view/AddNotification.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <x-header :left-options="{showBack:true,backText:'',}">
+            <span>通知</span>
+            <i slot="right" @click="on_plus_btn_click" class="iconfont  icon-jiahao"></i>
+        </x-header>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AddNotification',
+}
+</script>
+
+<style>
+
+</style>
