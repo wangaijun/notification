65d6f4778e2bb47ab86c7ca004c7295dce01e514
wangaijun <hw_wangaijun@163.com>
2019-01-03 08:47:32 +0800

    成功提交通知

--- src/view/AddNotification.vue
+++ src/view/AddNotification.vue
@@ -68,20 +68,14 @@ export default {
                 createDate:1545997022000,
                 creatorId: "6458251148183884119",
                 creatorName: "罗少帅",
-                finishRead: 0,
-                isAdd:1,
                 isToAllMember:0,
                 noticeDate:1545996992000,
-                noticeId:1583100951398439,
-                noticeType:1,
+                noticeType:'1',
                 participants:"6298842148508439286,6225576885289685719",
                 remark:"备注",
-                resourceId:null,
                 responsibleId:"6458251148183884119",
-                responsibleName:"罗少帅",
-                status:1,
+                status:0,
                 subcontractorId:"1581062192135033",
-                updateDate:"1581062192135033",
             }
         }
     },
@@ -119,9 +113,29 @@ export default {
     methods:{
         on_ok_btn_click(){
             console.log('on click');
+            // let s = '{"content":"通知内容2","createDate":0,"creatorName":"罗少帅","isToAllMember":0,"noticeDate":1546474761857,"noticeType":"1","participants":"6298842148508439286","remark":"备注2","responsibleId":"6225576885289685719","status":"0","subcontractorId":"1581062207357819"}'
+            // let b = JSON.parse(s)
+            let b = this.obj
+            console.log(JSON.stringify(b));
+            // debugger
+            this.$axios
+            .post('api/v1/projects/1579013294676606/notice/save'
+            ,b)
+            .then(response => {
+                console.log(response);
+                if(response.data.code==0){
+                    this.$router.go(-1)
+                }
+                else{
+                    console.log(response.data.message);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         },
         on_plus_btn_click(){
-
+            
         },
         datechange(d){
             console.log(d);
